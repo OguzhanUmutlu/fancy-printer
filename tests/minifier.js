344f08ec5d8931f35b96cde16ec0ff935cb34806
Oğuzhan Umutlu <oguzhan.umutlu123@gmail.com>
2023-06-26 17:20:01 +0300

    bugfix

--- tests/minifier.js
+++ tests/minifier.js
@@ -1,8 +1,9 @@
 const fs = require("fs");
+const path = require("path");
 const uglify = require("uglify-js");
 const printer = require("../index");
 
 printer.clear();
 printer.info("Minifying...");
-fs.writeFileSync("../index.min.js", uglify.minify(fs.readFileSync("../index.js", "utf8")).code);
+fs.writeFileSync(path.join(__dirname, "../index.min.js"), uglify.minify(fs.readFileSync(path.join(__dirname, "../index.js"), "utf8")).code);
 printer.pass("Successfully minified!");
