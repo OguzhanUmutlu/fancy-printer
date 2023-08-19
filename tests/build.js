c422442261062e70b161ceb02d540da9b64e05e1
Oğuzhan <oguzhan.umutlu123@gmail.com>
2023-08-19 17:24:28 +0300

    Added module support for web

---
+++ tests/build.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const path = require("path");
+const uglify = require("uglify-js");
+const printer = require("../index");
+
+const T = Date.now();
+printer.clear();
+printer.info("Building...");
+const original = fs.readFileSync(path.join(__dirname, "../index.js"), "utf8");
+
+fs.writeFileSync(path.join(__dirname, "../index.min.js"), uglify.minify(original).code);
+
+const spl = original.split("/*@buildExport*/");
+const moduleCode = spl[0] + spl[2] + original
+    .replace("/*@buildModule", "")
+    .replace("@buildModule*/", "");
+fs.writeFileSync(
+    path.join(__dirname, "../index.module.js"),
+    moduleCode
+);
+
+fs.writeFileSync(path.join(__dirname, "../index.module.min.js"), uglify.minify(moduleCode).code);
+
+printer.pass("Successfully built in " + (Date.now() - T) + "ms!");
--- tests/minifier.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-const path = require("path");
-const uglify = require("uglify-js");
-const printer = require("../index");
-
-printer.clear();
-printer.info("Minifying...");
-fs.writeFileSync(path.join(__dirname, "../index.min.js"), uglify.minify(fs.readFileSync(path.join(__dirname, "../index.js"), "utf8")).code);
-printer.pass("Successfully minified!");
